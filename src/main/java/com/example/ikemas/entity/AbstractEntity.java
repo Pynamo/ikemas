915e1d32b25687cf7b9022c7502a4061c16b3508
hamada <koki.hamada87@gmail.com>
2023-08-21 22:46:51 +0900

    update

--- src/main/java/com/example/ikemas/entity/AbstractEntity.java
+++ src/main/java/com/example/ikemas/entity/AbstractEntity.java
@@ -1,4 +1,31 @@
 package com.example.ikemas.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Data;
+
+import java.util.Date;
+
+@MappedSuperclass
+@Data
 public class AbstractEntity {
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        Date date = new Date();
+        setCreatedAt(date);
+        setUpdatedAt(date);
+    }
+    @PreUpdate
+    public void onPreUpdate() {
+        setUpdatedAt(new Date());
+    }
 }
